aecb51aa9cebac93f618bbea489d37002270046a
M Saad Hanif <support@sprouto.com>
2026-05-13 20:44:19 +0500

    latest be for render

--- backend/src/utils/cookies.ts
+++ backend/src/utils/cookies.ts
@@ -1,32 +1,66 @@
-import { Response} from "express";
+// import { Response} from "express";
+
+// export const setAccessTokenCookie = (res: Response, token: string) => {
+//   res.cookie("AccessToken", token, {
+//       httpOnly: true,
+//       secure: false,  //true 
+//       sameSite: "lax", //none
+//       maxAge: 7 * 24 * 60 * 60 * 1000,
+//     });
+// };
+
+// export const setRefreshTokenCookie = (res: Response, token: string) => {
+//   res.cookie("RefreshToken", token, {
+//       httpOnly: true,
+//       secure: false,
+//       sameSite: "lax",
+//       maxAge: 7 * 24 * 60 * 60 * 1000,
+//     });
+// };
+
+// export const clearAuthCookies = (res: Response) => {
+//   res.clearCookie("AccessToken", {
+//       httpOnly: true,
+//       secure: false,
+//       sameSite: "lax"});
+  
+//       res.clearCookie("RefreshToken", {
+//       httpOnly: true,
+//       secure: false,
+//       sameSite: "lax"});
+// };
+
+
+
+
+import { Response } from "express";
+
+const cookieOptions = {
+  httpOnly: true,
+  secure: true,
+  sameSite: "none" as const,
+  maxAge: 7 * 24 * 60 * 60 * 1000,
+};
 
 export const setAccessTokenCookie = (res: Response, token: string) => {
-  res.cookie("AccessToken", token, {
-      httpOnly: true,
-      secure: false,  //true 
-      sameSite: "lax", //none
-      maxAge: 7 * 24 * 60 * 60 * 1000,
-    });
+  res.cookie("AccessToken", token, cookieOptions);
 };
 
 export const setRefreshTokenCookie = (res: Response, token: string) => {
-  res.cookie("RefreshToken", token, {
-      httpOnly: true,
-      secure: false,
-      sameSite: "lax",
-      maxAge: 7 * 24 * 60 * 60 * 1000,
-    });
+  res.cookie("RefreshToken", token, cookieOptions);
 };
 
 export const clearAuthCookies = (res: Response) => {
   res.clearCookie("AccessToken", {
-      httpOnly: true,
-      secure: false,
-      sameSite: "lax"});
-  
-      res.clearCookie("RefreshToken", {
-      httpOnly: true,
-      secure: false,
-      sameSite: "lax"});
+    httpOnly: true,
+    secure: true,
+    sameSite: "none",
+  });
+
+  res.clearCookie("RefreshToken", {
+    httpOnly: true,
+    secure: true,
+    sameSite: "none",
+  });
 };
 
